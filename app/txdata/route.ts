7138362d74db55d449677d704c48d3dc693745f6
Bryan Fullam <bryan@querfeld.bio>
2024-03-22 20:46:50 +0100

    Reduce test transaction value

--- app/txdata/route.ts
+++ app/txdata/route.ts
@@ -14,7 +14,7 @@ export async function POST(
     params: {
       abi: [],
       to: `0x${process.env.TO_ADDRESS}`,
-      value: "898347624297249",
+      value: "89834762429724",
     },
   });
 }
