f8e3e820003aa0c90f50ed773fb4986c15ab557c
Bryan Fullam <bryan@querfeld.bio>
2024-03-22 16:00:19 +0100

    Add frames.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
+import type { Metadata } from 'next';
+
 export const viewport = {
   width: 'device-width',
   initialScale: 1.0,
+};
+
+export const metadata: Metadata = {
+  title: 'Test Frame',
+  description: 'A test frame',
+  openGraph: {
+    title: 'Test Frame',
+    description: 'A test frame',
+    images: [`https://framesjs.org/og.png`],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,62 @@
-import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameReducer,
+  NextServerPageProps,
+  getFrameMessage,
+  getPreviousFrame,
+  useFramesReducer,
+} from "frames.js/next/server";
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: "Beginn"
-    }
-  ],
-  image: `https://framesjs.org/og.png`,
-  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
-});
-
-export const metadata: Metadata = {
-  title: 'Test Frame',
-  description: 'A test frame',
-  openGraph: {
-    title: 'Test Frame',
-    description: 'A test frame',
-    images: [`https://framesjs.org/og.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
+type State = {
+  recentButtonIndex: string | null;
+  total_button_presses: number;
 };
 
-export default function Page() {
+const initialState = { recentButtonIndex: null, total_button_presses: 0 };
+
+const reducer: FrameReducer<State> = (state, action) => {
+  console.log(state, action);
+  return {
+    total_button_presses: state.total_button_presses + 1,
+    recentButtonIndex: action.postBody?.untrustedData.buttonIndex
+    ? String(action.postBody?.untrustedData.buttonIndex)
+    : null,  };
+};
+
+export default async function Page({ searchParams }: NextServerPageProps) {
+  const previousFrame = getPreviousFrame<State>(searchParams);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody);
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+
+  const [state] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame
+  );
+
   return (
-    <>
-      <h1>Test Frame</h1>
-    </>
+    <div className="p-4">
+      <FrameContainer
+        postUrl="/frames"
+        pathname="/"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        <FrameImage aspectRatio="1:1">
+          <div tw="bg-purple-800 text-white w-full h-full justify-center items-center">
+            Awesome Product
+          </div>
+        </FrameImage>
+        <FrameButton action="tx" target="/txdata">
+          Buy now 🎁
+        </FrameButton>
+      </FrameContainer>
+    </div>
   );
 }
