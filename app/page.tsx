e29e83063e77cfbd933c81ce5f1a751a07e4baa7
Bryan Fullam <bryan@querfeld.bio>
2024-03-22 12:35:58 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Begin"
+    }
+  ],
+  image: `https://framesjs.org/og.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+});
+
+export const metadata: Metadata = {
+  title: 'Test Frame',
+  description: 'A test frame',
+  openGraph: {
+    title: 'Test Frame',
+    description: 'A test frame',
+    images: [`https://framesjs.org/og.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Test Frame</h1>
+    </>
+  );
+}
