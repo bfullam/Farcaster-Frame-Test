5ba147281f114fc7b303eb55988f580a4a789adc
Bryan Fullam <bryan@querfeld.bio>
2024-03-22 20:34:05 +0100

    Fix to_address env variable

--- app/txdata/route.ts
+++ app/txdata/route.ts
@@ -4,12 +4,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(
   req: NextRequest
 ): Promise<NextResponse<TransactionTargetResponse>> {
+  if (process.env.TO_ADDRESS === undefined) {
+    throw new Error("Missing TO_ADDRESS environment variable");
+  }
+
   return NextResponse.json({
     chainId: "eip155:84532", // Base Sepolia Testnet
     method: "eth_sendTransaction",
     params: {
       abi: [],
-      to: process.env.TO_ADDRESS,
+      to: `0x${process.env.TO_ADDRESS}`,
       value: "50000000000000000",
     },
   });
