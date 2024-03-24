b031e7c32e244811ccdd2e53b53c9d043cb32eac
rikkce <l.riccardo@web.de>
2024-03-24 11:44:16 +0100

    fixed tailwind configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 export const metadata = {
-  title: 'Ecommerce Frame',
-  description: '...',
-}
+  title: "Ecommerce Frame",
+  description: "...",
+};
+
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,38 @@
-import { put } from '@vercel/blob';
-import { sql } from '@vercel/postgres';
-import { revalidatePath } from 'next/cache';
- 
-export default async function Home() {
-    async function submitForm(formData: FormData) {
-        'use server';
-        // Upload image to blob storage
-        const imageFile = formData.get('image') as File;
-        const blob = await put(imageFile.name, imageFile, {
-            access: 'public',
-        });
-        revalidatePath('/');
+import { put } from "@vercel/blob";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
 
-        // Add DB entry with image URL and form data
-        const receivingWallet = formData.get('wallet') as string;
-        const price = formData.get('price') as string;
-        const frameInsertionResult = await sql`INSERT INTO FRAMES (imageurl, receivingwallet, price) VALUES (${blob.url}, ${receivingWallet}, ${price}) RETURNING *;`;
-        console.log(frameInsertionResult.rows[0]);
-    }
-     
-    return (
+export default async function Home() {
+  async function submitForm(formData: FormData) {
+    "use server";
+    // Upload image to blob storage
+    const imageFile = formData.get("image") as File;
+    const blob = await put(imageFile.name, imageFile, {
+      access: "public",
+    });
+    revalidatePath("/");
+
+    // Add DB entry with image URL and form data
+    const receivingWallet = formData.get("wallet") as string;
+    const price = formData.get("price") as string;
+    const frameInsertionResult =
+      await sql`INSERT INTO FRAMES (imageurl, receivingwallet, price) VALUES (${blob.url}, ${receivingWallet}, ${price}) RETURNING *;`;
+    console.log(frameInsertionResult.rows[0]);
+  }
+
+  return (
     <form action={submitForm}>
-        <h1>Upload Image</h1>
-        <input type="file" id="image" name="image" required />
-        <h1>Set Wallet Address</h1>
-        <input type="text" id="wallet" name="wallet"></input>
-        <h1>Set Product Price in ETH</h1>
-        <input type="text" id="price" name="price"></input>
-        <br />
-        <br />
-        <br />
-        <button type='submit'>Submit</button>
+      <h1 className="text-sm font-bold underline">Hello world!</h1>
+      <h1 className="flex">Upload Image</h1>
+      <input type="file" id="image" name="image" required />
+      <h1>Set Wallet Address</h1>
+      <input type="text" id="wallet" name="wallet"></input>
+      <h1>Set Product Price in ETH</h1>
+      <input type="text" id="price" name="price"></input>
+      <br />
+      <br />
+      <br />
+      <button type="submit">Submit</button>
     </form>
-    );
+  );
 }
