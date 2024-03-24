8f8609a535e6eb7b602d48d2f3eb2429420799d7
Bryan Fullam <bryanfullam@gmail.com>
2024-03-24 15:59:00 +0100

    Move dynamic context provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export const metadata = {
-  title: "Ecommerce Frame",
-  description: "...",
-};
 
+"use client";
+import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react-core";
 import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
 export default function RootLayout({
   children,
@@ -14,29 +13,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex flex-row items-center pl-10 p-3 space-x-5 border border-b border-gray-300">
-          <div className="flex flex-row items-center space-x-1">
-            <Image
-              src="/flames.jpg" // The path to your image
-              alt="Description of the image" // Alternative text for the image
-              width={40} // Desired width of the image (can be changed)
-              height={20} // Desired height of the image (can be changed)
-            />
-            <div className="text-lg font-semibold">Flames</div>
-          </div>
+      <DynamicContextProvider 
+        settings={{ 
+        environmentId: '7c8a7b2a-5faf-4b2f-9acb-0720d58dafa2',
+        walletConnectors: [ EthereumWalletConnectors ],
+      }}>
+        <DynamicWidget />
+        <body>
+          <div className="flex flex-row items-center pl-10 p-3 space-x-5 border border-b border-gray-300">
+            <div className="flex flex-row items-center space-x-1">
+              <Image
+                src="/flames.jpg" // The path to your image
+                alt="Description of the image" // Alternative text for the image
+                width={40} // Desired width of the image (can be changed)
+                height={20} // Desired height of the image (can be changed)
+              />
+              <div className="text-lg font-semibold">Flames</div>
+            </div>
 
-          <Link href="/" passHref>
-            <span className="font-semibold pl-5">Create</span>{" "}
-            {/* Add Link and a tags */}
-          </Link>
-          <Link href="/manage" passHref>
-            <span className="font-semibold">Manage</span>{" "}
-            {/* Add Link and a tags */}
-          </Link>
-        </div>
-        <div>{children}</div>
-      </body>
+            <Link href="/" passHref>
+              <span className="font-semibold pl-5">Create</span>{" "}
+              {/* Add Link and a tags */}
+            </Link>
+            <Link href="/manage" passHref>
+              <span className="font-semibold">Manage</span>{" "}
+              {/* Add Link and a tags */}
+            </Link>
+          </div>
+          <div>{children}</div>
+        </body>
+      </DynamicContextProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-"use client";
-import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import EcomForm from './components/EcomForm';
+
+export const metadata = {
+title: "Ecommerce Frame",
+description: "...",
+};
  
 export default async function Home(submitForm: any) {     
     return (
-        <DynamicContextProvider 
-        settings={{ 
-          environmentId: '7c8a7b2a-5faf-4b2f-9acb-0720d58dafa2',
-          walletConnectors: [ EthereumWalletConnectors ],
-        }}>
-            <DynamicWidget />
-            <EcomForm/>
-        </DynamicContextProvider> 
+        <EcomForm/>
     );
 }
